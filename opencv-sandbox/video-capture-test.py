108eb279cb5bd39673cd9eb4bb548c6a2be7115b
renneru <renneruonkoi@gmail.com>
2020-07-28 00:50:57 +0900

    Modify test script to add red 400x400 square overlay.

--- opencv-sandbox/video-capture-test.py
+++ opencv-sandbox/video-capture-test.py
@@ -8,12 +8,19 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv_img = cv2.rectangle(frame, (121, 41), (520, 440), (0, 0, 255), 1)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #cv2.imshow('frame',gray)
+    cv2.imshow('frame',cv_img)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
+    elif key == ord(' '):
+        print(cv_img[40:440, 120:520].shape)
+        print("Space bar pressed.")
 
 # When everything done, release the capture
 cap.release()
