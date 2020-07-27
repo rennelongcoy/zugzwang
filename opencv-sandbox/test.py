1c6e7dcb9b7775687f6fb8cf196cc3115c45d4b9
renneru <renneruonkoi@gmail.com>
2020-07-28 00:31:26 +0900

    Add Python 3 scripts to test web cam connection.

---
+++ opencv-sandbox/test.py
@@ -0,0 +1,11 @@
+import cv2
+# load the input image and show its dimensions, keeping in mind that
+# images are represented as a multi-dimensional NumPy array with
+# shape no. rows (height) x no. columns (width) x no. channels (depth)
+image = cv2.imread("sample_00.jpg")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+# display the image to our screen -- we will need to click the window
+# open by OpenCV and press a key on our keyboard to continue execution
+cv2.imshow("Image", image)
+cv2.waitKey(0)
