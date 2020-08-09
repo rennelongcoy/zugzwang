2fc26d84d8d4e7ce0530772efac50a01705cee0f
renneru <renneruonkoi@gmail.com>
2020-08-09 13:16:33 +0900

    Updated boardstate module to include inference.

--- modules/boardstate.py
+++ modules/boardstate.py
@@ -1,16 +1,55 @@
 import chess
 import chess.pgn
 import numpy as np
+import tflite_runtime
+import tflite_runtime.interpreter as tflite
 
 class BoardState:
     def __init__(self):
         self.board = chess.Board()
         self.game = chess.pgn.Game()
-        self.isStartOfGame = True
+        self.isFirstMove = True
         self.node = 0
+        self.prev_state = np.array([[ 1,  1,  1,  1,  1,  1,  1,  1],  # Rank A
+                                    [ 1,  1,  1,  1,  1,  1,  1,  1],  # Rank B
+                                    [ 0,  0,  0,  0,  0,  0,  0,  0],  # Rank C
+                                    [ 0,  0,  0,  0,  0,  0,  0,  0],  # Rank D
+                                    [ 0,  0,  0,  0,  0,  0,  0,  0],  # Rank E
+                                    [ 0,  0,  0,  0,  0,  0,  0,  0],  # Rank F
+                                    [-1, -1, -1, -1, -1, -1, -1, -1],  # Rank G
+                                    [-1, -1, -1, -1, -1, -1, -1, -1]]) # Rank H
+        self.move_num = 1
+        self.current_state = self.prev_state
+        self.interpreter = tflite.Interpreter(model_path="/zugzwang/model/model.tflite")
+        self.interpreter.allocate_tensors()
+        
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+        
+        #print(input_details[0]['dtype'])
+        self.count = self.input_details[0]['shape'][0] # Only 1 image to be input
+        self.height = self.input_details[0]['shape'][1]
+        self.width = self.input_details[0]['shape'][2]
+        self.depth = self.input_details[0]['shape'][3]
+        #print("Expected input count  = " + str(count))
+        #print("Expected input height = " + str(height))
+        #print("Expected input width  = " + str(width))
+        #print("Expected input depth  = " + str(depth))
 
-    def getBoardStateDiff(self, current_state, prev_state):
-        state_diff = current_state - prev_state
+        #print(output_details[0]['dtype'])
+        self.rows = self.output_details[0]['shape'][0]
+        self.cols = self.output_details[0]['shape'][1]
+        #print("Expected output rows = " + str(rows))
+        #print("Expected output cols  = " + str(cols))
+
+    def setCurrentState(self, current_state):
+        self.current_state = current_state
+
+    def setPrevState(self, prev_state):
+        self.prev_state = prev_state
+
+    def getBoardStateDiff(self):
+        state_diff = self.current_state - self.prev_state
         if self.board.turn == chess.BLACK:
             state_diff = -state_diff
         return state_diff
@@ -40,12 +79,54 @@ class BoardState:
         return (move, uci_move, san_move)
 
     def executeMove(self, move):
-        if self.isStartOfGame:
+        if self.isFirstMove:
             self.node = self.game.add_variation(move)
-            self.isStartOfGame = False
+            self.isFirstMove = False
         else:
             self.node = self.node.add_variation(move)
         self.board.push(move)
 
     def isWhitesTurn(self):
         return self.board.turn == chess.WHITE
+
+    def getStateFromImage(self, raw_sample_frame):
+        temp = np.array([[ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0],
+                        [ 0,  0,  0,  0,  0,  0,  0,  0]])
+
+        # Size (HxWxD) = (50x50x3)
+        cv_img_400x400 = raw_sample_frame[40:440, 120:520]
+
+        # Split current frame to 8x8 individual squares
+        for i in range(0, 351, 50):
+            for j in range(0, 351, 50):
+                # Add N dimension to be (NxHxWxD) = (1x50x50x3)
+                one_square = np.expand_dims(cv_img_400x400[i:i+50, j:j+50, :], axis=0)
+
+                # Infer piece color in each square by using TF Lite model (start from Rank A to Rank H)
+                self.interpreter.set_tensor(self.input_details[0]['index'], one_square)
+                self.interpreter.invoke()
+                output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+                results = np.round(np.squeeze(output_data))
+
+                # Map neural network softmax output to corresponding class
+                if results[0] == 1:
+                    temp[i//50, j//50] = -1 # black
+                elif results[1] == 1:
+                    temp[i//50, j//50] = 0  # empty
+                elif results[2] == 1:
+                    temp[i//50, j//50] = 1  # white
+
+        # Rotate 90-degrees clockwise thrice if White is on right side
+        # TODO: # Rotate 90-degrees clockwise once if White is on left side
+        temp = np.rot90(m=temp, k=3)
+
+        # Flip in up-down direction to match initial_state
+        current_state = np.flipud(temp)
+
+        return current_state
--- zugzwang.py
+++ zugzwang.py
@@ -1,59 +1,21 @@
 import numpy as np
 import cv2
-import tflite_runtime
-import tflite_runtime.interpreter as tflite
 
 from modules import boardstate
+
+print("zugzwang v0.01")
+#print("numpy.__version__          = " + np.__version__)
+#print("cv2.__version__            = " + cv2.__version__)
+#print("tflite_runtime.__version__ = " + tflite_runtime.__version__)
 
 if __name__ == "__main__":
     boardState = boardstate.BoardState()
 
-    interpreter = tflite.Interpreter(model_path="model/model.tflite")
-    interpreter.allocate_tensors()
-    
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    
-    #print(input_details[0]['dtype'])
-    count = input_details[0]['shape'][0] # Only 1 image to be input
-    height = input_details[0]['shape'][1]
-    width = input_details[0]['shape'][2]
-    depth = input_details[0]['shape'][3]
-    #print("Expected input count  = " + str(count))
-    #print("Expected input height = " + str(height))
-    #print("Expected input width  = " + str(width))
-    #print("Expected input depth  = " + str(depth))
-    
-    #print(output_details[0]['dtype'])
-    rows = output_details[0]['shape'][0]
-    cols = output_details[0]['shape'][1]
-    #print("Expected output rows = " + str(rows))
-    #print("Expected output cols  = " + str(cols))
-    
     videoCapture = cv2.VideoCapture(0)
-    
-    #print("zugzwang v0.01")
-    #print("chess.__version__          = " + chess.__version__)
-    #print("numpy.__version__          = " + np.__version__)
-    #print("cv2.__version__            = " + cv2.__version__)
-    #print("tflite_runtime.__version__ = " + tflite_runtime.__version__)
-
-    # Populate by row from A-rank (1st row) to H-rank
-    initial_state = np.array([[ 1,  1,  1,  1,  1,  1,  1,  1],
-                            [ 1,  1,  1,  1,  1,  1,  1,  1],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [-1, -1, -1, -1, -1, -1, -1, -1],
-                            [-1, -1, -1, -1, -1, -1, -1, -1]])
-
-    prev_state = initial_state
-    move_num = 1
 
     print("Press 'esc' to quit. Press ' ' to make a move.")
     print("Chess Game Record:")
-
+    move_num = 1
     while(True):
         # Capture frame-by-frame
         ret, frame = videoCapture.read()
@@ -69,47 +31,11 @@ if __name__ == "__main__":
         if key == 27: # Esc key
             break
         elif key == ord(' '): # Space bar
-            temp = np.array([[ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0],
-                            [ 0,  0,  0,  0,  0,  0,  0,  0]])
-
-            # Size (HxWxD) = (50x50x3)
-            cv_img_400x400 = raw_sample_frame[40:440, 120:520]
-
-            # Split current frame to 8x8 individual squares
-            for i in range(0, 351, 50):
-                for j in range(0, 351, 50):
-                    # Add N dimension to be (NxHxWxD) = (1x50x50x3)
-                    one_square = np.expand_dims(cv_img_400x400[i:i+50, j:j+50, :], axis=0)
-
-                    # Infer piece color in each square by using TF Lite model (start from Rank A to Rank H)
-                    interpreter.set_tensor(input_details[0]['index'], one_square)
-                    interpreter.invoke()
-                    output_data = interpreter.get_tensor(output_details[0]['index'])
-                    results = np.round(np.squeeze(output_data))
-
-                    # Map neural network softmax output to corresponding class
-                    if results[0] == 1:
-                        temp[i//50, j//50] = -1 # black
-                    elif results[1] == 1:
-                        temp[i//50, j//50] = 0  # empty
-                    elif results[2] == 1:
-                        temp[i//50, j//50] = 1  # white
-
-            # Rotate 90-degrees clockwise thrice if White is on right side
-            # TODO: # Rotate 90-degrees clockwise once if White is on left side
-            temp = np.rot90(m=temp, k=3)
-
-            # Flip in up-down direction to match initial_state
-            current_state = np.flipud(temp)
+            current_state = boardState.getStateFromImage(raw_sample_frame)
 
             # Calculate performed move
-            state_diff = boardState.getBoardStateDiff(current_state, prev_state)
+            boardState.setCurrentState(current_state)
+            state_diff = boardState.getBoardStateDiff()
             start_square = boardState.getStartSquare(state_diff)
             dest_square = boardState.getDestSquare(state_diff)
             (move, uci_move, san_move) = boardState.convertToChessMove(start_square, dest_square)
@@ -125,7 +51,7 @@ if __name__ == "__main__":
             boardState.executeMove(move)
 
             # Update previous state
-            prev_state = current_state
+            boardState.setPrevState(current_state)
 
     # Turn off camera capture when game is over
     print("\n")
